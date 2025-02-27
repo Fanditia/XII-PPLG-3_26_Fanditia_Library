48379f1eeb61e237a1e942b9d92bdc9f01a2c0e3
fanditia <fanditiasurya@gmail.com>
2025-02-27 23:14:16 +0700

    update-category

--- models/categories.js
+++ models/categories.js
@@ -2,19 +2,19 @@ const db = require('../config/db');
 
 const Category = {
   getAll: (callback) => {
-    db.query('SELECT * FROM kategori', callback);
+    db.query('SELECT * FROM categories', callback);
   },
 
   add: (name, callback) => {
-    db.query('INSERT INTO kategori (name) VALUES (?)', [name], callback);
+    db.query('INSERT INTO categories (name) VALUES (?)', [name], callback);
   },
 
   update: (id, name, callback) => {
-    db.query('UPDATE kategori SET name = ? WHERE id = ?', [name, id], callback);
+    db.query('UPDATE categories SET name = ? WHERE id = ?', [name, id], callback);
   },
 
   delete: (id, callback) => {
-    db.query('DELETE FROM kategori WHERE id = ?', [id], callback);
+    db.query('DELETE FROM categories WHERE id = ?', [id], callback);
   },
 };
 
